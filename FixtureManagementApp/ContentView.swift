ac7d895d4e5cffbf893fd4f2d810e688cd7e859d
saitodaisuke <saito@2ndwave.jp>
2021-05-10 14:07:16 +0900

    Initial Commit

---
+++ FixtureManagementApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FixtureManagementApp
+//
+//  Created by 齋藤 大輔 on 2021/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FixtureManagementApp/FixtureManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixtureManagementAppApp.swift
+//  FixtureManagementApp
+//
+//  Created by 齋藤 大輔 on 2021/05/10.
+//
+
+import SwiftUI
+
+@main
+struct FixtureManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
